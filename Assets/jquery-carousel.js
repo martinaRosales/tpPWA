e0e62a89a6fe284c5e25f7ca3a263b41ec50ccf5
Martina Rosales <martina.rosales@est.fi.uncoma.edu.ar>
2023-04-09 01:32:57 -0300

    otro intento de carrusel

--- Assets/jquery-carousel.js
+++ Assets/jquery-carousel.js
@@ -2,19 +2,17 @@ var multipleItemCarousel = document.querySelector('#carouselExampleControls');
 
 
 if(window.matchMedia("(min-width:576px)").matches){
-
+    console.log(window.matchMedia("(min-width:576px)").matches)
     var carouselWidth = $('.carousel-inner')[0].scrollWidth;
     var cardWidth = $('.carousel-item').width();
     
     var scrollPosition = 0;
     
     $('.carousel-control-next').on('click', function(){
-        if(scrollPosition < (carouselWidth - (cardWidth * 8))) {
+        if(scrollPosition < (carouselWidth - (cardWidth * 9))) {
             scrollPosition = scrollPosition + cardWidth;
             $('.carousel-inner').animate({scrollLeft: scrollPosition},600);
-        } else {
-           
-        }
+        } 
     });
     
     $('.carousel-control-prev').on('click', function(){
@@ -24,5 +22,6 @@ if(window.matchMedia("(min-width:576px)").matches){
         } 
     });
 } else {
+    console.log(window.matchMedia("(min-width:576px)").matches)
     $(multipleItemCarousel).addClass('slide');
 }
