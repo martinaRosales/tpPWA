8960996618c42c87174500bdfa45cff5dca493a2
jeronimo-coder <hanapuca.jr@gmail.com>
2023-04-09 19:21:31 -0300

    Manejo de info con array

---
+++ Assets/manejoInfo.js
@@ -0,0 +1,25 @@
+// Obtener todas las imágenes del carrusel
+const carruselImagenes = document.querySelectorAll('#recipeCarousel .carousel-item .img-fluid');
+
+// Agregar un evento "click" a cada imagen del carrusel
+carruselImagenes.forEach(function(imagen) {
+  imagen.addEventListener('click', function() {
+    // Obtener el índice de la imagen haciendo referencia al atributo de "data-index"
+    const index = this.getAttribute('data-index');
+    
+    // Obtener la información relacionada del objeto "infoImagenesElite" usando el índice
+    const info = infoImagenesElite[index];
+    
+    // Mostrar la información en el otro div
+    const otroDiv = document.querySelector('#otroDiv');
+    otroDiv.innerHTML = `
+      <h3>${info.titulo}</h3>
+      <p>${info.descripcion}</p>
+    `;
+    const imagenDiv = document.querySelector('#imagenDiv');
+    imagenDiv.innerHTML = `
+      <h3>${info.titulo}</h3>
+      <img src="${info.imagen}">
+    `;
+  });
+});
